820483bb2c51718c14f2c332e6d2f7c7a47558a2
Tabitha <tks2419@gmail.com>
2020-08-02 23:37:54 -0700

    gradient boosting and interpolation helper functions

---
+++ Interpolation/interpolation.py
@@ -0,0 +1,143 @@
+import pandas as pd
+import numpy as np
+import matplotlib.pyplot as plt
+from haversine import haversine, Unit
+from sklearn.model_selection import train_test_split
+
+"""
+Uses regex to generate a street name column and house number column from address, modifies the original dataframe
+df: data frame
+address: name of column where address listed as house number and street name
+returns: None, dataframe is modified in place
+"""
+def create_street_house(df, address):
+    df[["house_number", "street_name"]] = df[address].str.split(" ", 1, expand = True)
+    df["house_number"] = df["house_number"].str.extract(r'(\d+)')#.astype('int64', errors = 'ignore') #deal with numbers when they are listed as 124-125 (choose the first one) there's also one that's 2A which becomes 2
+    df["house_number"] = df["house_number"].apply(pd.to_numeric, errors='coerce').dropna()
+
+"""
+Generates columns needed to create sequence id based on distance, modifying original dataframe
+df: dataframe with unique dwellings
+X: name of column with latitude
+Y: name of column with longitude
+Address: (optional) name of column with address, if included creates a column with the next address
+Ward: (optional) name of column with ward number, if included creates a column with the next ward
+returns: None, df is modified in place
+"""
+def col_for_seq(df, X, Y, Address = None, Ward = None):
+    df["dwelling_num_listed"] = df.index
+    df["next_dnl"] = df["dwelling_num_listed"].shift(-1)
+    df["next_x"] = df[X].shift(-1)
+    df["next_y"] = df[Y].shift(-1)
+
+    if Address:
+        df["next_address"] = df[Address].shift(-1)
+    if Ward:
+        df["next_ward"] = df[Ward].shift(-1)
+
+    df['dist'] = df.apply(lambda row: haversine((row[X], row[Y]), (row["next_x"], row["next_y"]), unit=Unit.MILES), axis=1)
+    df["num_between"] = df["next_dnl"] - df["dwelling_num_listed"] #really number between plus one
+
+"""
+Creates train and test data
+data_1880: 1880 dataframe
+data_1850: 1850 dataframe
+cols: list of columns to include for training dataset
+both: boolean, if True creates training data that combines 1880 and 1850 dataset
+returns: list of train_X, train_Y, test_1880_X, test_1880_y, test_1850_X, test_1850_y
+"""
+def create_train_test_data(data_1880, data_1850, cols, both = True):
+    data_1880 = data_1880.dropna(subset = ["house_number"]).fillna("None")
+    data_1850 = data_1850.dropna(subset = ["house_number"]).fillna("None")
+    train_1880_X, test_1880_X, train_1880_y, test_1880_y = train_test_split(data_1880.loc[:,cols], data_1880.loc[:,"house_number"], random_state = 23)
+
+    if not both:
+        return [train_1880_X, train_1880_y, test_1880_X, test_1880_y, data_1850.loc[:,cols], data_1850.loc[:,"house_number"]]
+    else:
+        train_1850_X, test_1850_X, train_1850_y, test_1850_y = train_test_split(data_1850.loc[:, cols],
+                                                                                data_1850.loc[:, "house_number"], random_state = 23)
+        return [pd.concat([train_1880_X, train_1850_X]), pd.concat([train_1880_y, train_1850_y]), test_1880_X, test_1880_y, test_1850_X, test_1850_y]
+
+"""
+Creates sequence id based on distance between consecutive dwellings
+df: dataframe containing unique dwellings
+d: maximum distance between dwellings, anything larger than this means a sequence break
+returns list: minimum sequence length, maximum sequence length, and dataframe with sequence_id and sequence_len olumns
+"""
+def get_dist_seq(df, d):
+    df = df.copy()
+    df["sequence_id"] = np.where(df["dist"] > d, df["dist"].index, np.nan)
+    df["sequence_id"].bfill(inplace = True)
+    df["sequence_len"] = df.groupby("sequence_id")["num_between"].transform('sum')
+    return [min(df["sequence_len"]), max(df["sequence_len"]), df]
+
+"""
+graphs relative features importances on a horizontal bar chart
+features: list of features in order
+coefs: corresponding coefficients (relative importance)
+title: Name of graph
+returns None, but prints graph
+"""
+def graph_coefs(features, coefs, title):
+    fig, ax = plt.subplots(1, figsize=(20, 10))
+    ax.barh(features, coefs)
+    ax.set_title(title)
+    ax.set_xlabel("Coefficients")
+    ax.set_ylabel("Features")
+    plt.show()
+
+"""
+Takes whole data set and dwellings level data set and joins them
+all: dataset with all entries
+cols: list of column names to include from dwellings level dataset
+joins: list of columns to perform the join on
+return: dataframe of all census with dwellings info
+"""
+def dwellings_to_all(all, dwellings, cols, joins):
+    df = all.merge(dwellings.loc[:,cols], how = "left", on = joins, validate = 'many_to_one')
+    #df.dropna(subset = ["house_number"], inplace = True)
+    return df
+
+"""
+This takes a dataframe of a single sequence and creates a sequence_order column, designed to be used with groupby("sequence_id").apply()
+df: dataframe with a single sequence, including "num_between" column
+returns: dataframe with sequence_order column
+"""
+
+def sequence_order(df):
+    df["sequence_order"] = df["num_between"].cumsum()
+    return df
+
+"""
+Takes the census dataframes and processes them to create dataframes ready to use for modeling
+census_1850: dataframe with 1850 census information
+census_1880: dataframe with 1880 census information
+returns: dwellings and entry level dataframes for both 1880 and 1850
+"""
+
+def sequence_datasets(census_1850, census_1880):
+    census_1850 = census_1850.dropna(subset=["CENSUS_DWELLING_NUM"]).copy()
+    dwellings_1850 = census_1850.groupby(["WARD_NUM", "CENSUS_DWELLING_NUM"], as_index=False).first()
+    dwellings_1880 = census_1880.drop_duplicates(subset=["CENSUS_ADDRESS"]).copy()
+
+    col_for_seq(dwellings_1850, "CD_X", "CD_Y", Address=None, Ward=None)
+    col_for_seq(dwellings_1880, "POINT_X", "POINT_Y", Address=None, Ward=None)
+
+    dwellings_1850 = get_dist_seq(dwellings_1850, 0.15)[2]
+    dwellings_1880 = get_dist_seq(dwellings_1880, 0.15)[2]
+
+    dwellings_1850.groupby("sequence_id").apply(sequence_order)
+    dwellings_1880.groupby("sequence_id").apply(sequence_order)
+
+    census_1880_model = dwellings_to_all(census_1880, dwellings_1880,
+                                                       ["CENSUS_MATCH_ADDR", "sequence_id", "sequence_order"] , ["CENSUS_MATCH_ADDR"])
+    census_1850_model = dwellings_to_all(census_1850, dwellings_1850,
+                                                       ["WARD_NUM", "CENSUS_DWELLING_NUM", "sequence_id", "sequence_order"],
+                                                       ["WARD_NUM", "CENSUS_DWELLING_NUM"])
+
+    create_street_house(dwellings_1880, "CENSUS_ADDRESS")
+    create_street_house(dwellings_1850, "CD_ADDRESS")
+    create_street_house(census_1880_model, "CENSUS_ADDRESS")
+    create_street_house(census_1850_model, "CD_ADDRESS")
+
+    return [dwellings_1850, dwellings_1880, census_1850_model, census_1880_model]
--- disambiguation/__init__.py
+++ disambiguation/__init__.py
@@ -3,6 +3,7 @@ import numpy as np
 import disambiguation.processing as dp
 import disambiguation.disambiguation as dl
 import disambiguation.analysis as da
+import disambiguation.benchmark as bm
 
 class Disambiguator:
 
@@ -107,3 +108,75 @@ class Disambiguator1880(Disambiguator):
     def get_under12_selections(self, age='CENSUS_AGE'):
         
         return da.get_under12_selections(self.results, age=age)
+
+class Benchmark():
+
+    def __init__(self, match, census, cd):
+
+        #Format lat/long for census data
+        self.census = self.set_census(census)
+        self.cd = self.set_cd(cd)
+        self.match = match
+        self.benchmark = self.create_benchmark()
+
+        #Variables to set
+        self.disambiguated = None
+        self.confidence = None
+
+        #outputs
+        self.benchmark_results = None
+        self.confusion_matrix = None
+
+    def set_census(self, census):
+        census = census.loc[:,['CENSUS_MATCH_ADDR', 'CENSUS_X', 'CENSUS_Y']].drop_duplicates()  # select diff variables
+        census.loc[census.CENSUS_Y > 1000, 'CENSUS_Y'] = 40.799935
+        return census
+
+    def set_cd(self, cd):
+        return cd[['OBJECTID', 'LONG', 'LAT', "CD_BLOCK_NUM"]]
+
+    def set_disambiguated(self, disambiguated):
+        self.disambiguated = disambiguated
+
+    def set_confidence(self, confidence):
+        self.confidence = confidence
+        # Need to figure out what's going on here
+        self.benchmark['confidence_score'] = self.benchmark['add_match'] #+ self.benchmark[self.confidence]
+
+    def create_benchmark(self):
+
+        benchmark = self.match.merge(self.census, how='left', on='CENSUS_MATCH_ADDR', validate='many_to_one')
+
+        benchmark['cd_hn'] = benchmark.apply(lambda row: bm.get_hn(row.MATCH_ADDR), axis=1)
+        benchmark['cen_hn'] = benchmark.apply(lambda row: bm.get_hn(row.CENSUS_MATCH_ADDR), axis=1)
+        benchmark['cd_add_cln'] = benchmark.apply(lambda row: bm.get_st(row.MATCH_ADDR), axis=1)
+        benchmark['cen_add_cln'] = benchmark.apply(lambda row: bm.get_st(row.CENSUS_MATCH_ADDR), axis=1)
+
+        benchmark['add_match'] = np.where(benchmark.cd_hn == benchmark.cen_hn, 0.1, 0) + np.where(
+            benchmark.cen_add_cln == benchmark.cd_add_cln, 0.9, 0)
+
+        benchmark.merge(self.cd, how='left', on='OBJECTID', validate='many_to_one')
+
+        return benchmark
+
+    def run_benchmarking(self):
+        if self.confidence is None:
+            raise Exception("Please set confidence first")
+        if self.disambiguated is None:
+            raise Exception("Please set disambiguated first")
+
+        self.benchmark_results = da.get_dist_based_match(self.benchmark)['results']
+        #self.confusion_matrix = da.compare_selections(self.disambiguated, self.benchmark_results)['confusion_matrix']
+
+    def get_benchmark(self):
+        return self.benchmark
+
+    def get_benchmark_results(self):
+        if self.benchmark_results is None:
+            raise Exception("Please run benchmarking first")
+        return self.benchmark_results
+
+    def get_confusion_matrix(self):
+        if self.confusion_matrix is None:
+            raise Exception("Please run benchmarking first")
+        return self.confusion_matrix
